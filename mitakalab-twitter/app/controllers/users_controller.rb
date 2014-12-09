abffbfa0e274b7aa494f39e5bee1d41f86d047a9
ryohasegawa <zaqopqqq@gmail.com>
2014-12-10 00:24:34 +0900

    finish

--- mitakalab-twitter/app/controllers/users_controller.rb
+++ mitakalab-twitter/app/controllers/users_controller.rb
@@ -2,18 +2,7 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
-    @user = Hash.new
-    if params[:username] == 'ryooopan'
-        @user[:name] = 'Ryo Suzuki'
-        @user[:username] = 'ryooopan'
-        @user[:location] = 'Kanagawa, Japan'
-        @user[:about] = 'Hello, I am Ryo.'
-    elsif params[:username] == 'moyahima'
-        @user[:name] = 'Shohei Aoki'
-        @user[:username] = 'moyahima'
-        @user[:location] = 'Tottori, Japan'
-        @user[:about] = 'Nice to meet you.'
-    end
-  end
+　def show
+    @user = User.find_by(:username => params[:username])
+　end
 end
