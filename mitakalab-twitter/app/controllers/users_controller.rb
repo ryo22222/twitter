fa46911d974f17880f91fda049fdf72add47bdd2
ryohasegawa <zaqopqqq@gmail.com>
2014-12-09 23:43:08 +0900

    rails g model user name:string username:string location:string about:text

--- mitakalab-twitter/app/controllers/users_controller.rb
+++ mitakalab-twitter/app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+    if params[:username] == 'ryooopan'
+        @user[:name] = 'Ryo Suzuki'
+        @user[:username] = 'ryooopan'
+        @user[:location] = 'Kanagawa, Japan'
+        @user[:about] = 'Hello, I am Ryo.'
+    elsif params[:username] == 'moyahima'
+        @user[:name] = 'Shohei Aoki'
+        @user[:username] = 'moyahima'
+        @user[:location] = 'Tottori, Japan'
+        @user[:about] = 'Nice to meet you.'
+    end
   end
 end
